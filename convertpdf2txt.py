779579f6b4a21df2267e4e593bf68fa112fc7522
sarthaksharma02 <50519719+sarthaksharma02@users.noreply.github.com>
2019-07-04 16:17:28 +0530

    PDF to Text Converter Program

---
+++ convertpdf2txt.py
@@ -0,0 +1,49 @@
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from io import StringIO
+
+pdf_path = '''path of the file you want to convert'''
+
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as fh:
+        # iterate over all pages of PDF document
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+            # creating a resoure manager
+            resource_manager = PDFResourceManager()
+            
+            # create a file handle
+            fake_file_handle = StringIO()
+            
+            # creating a text converter object
+            converter = TextConverter(
+                                resource_manager, 
+                                fake_file_handle, 
+                                codec='utf-8', 
+                                laparams=LAParams()
+                        )
+
+            # creating a page interpreter
+            page_interpreter = PDFPageInterpreter(
+                                resource_manager, 
+                                converter
+                            )
+
+            # process current page
+            page_interpreter.process_page(page)
+            
+            # extract text
+            text = fake_file_handle.getvalue()
+            yield text
+
+            # close open handles
+            converter.close()
+            fake_file_handle.close()
+
+text = ''
+# calling above function and extracting text
+for page in extract_text_from_pdf(pdf_path):
+    text +=  page
+
+print(text)
